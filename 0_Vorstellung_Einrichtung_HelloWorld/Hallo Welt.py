d628bf33758967bf012bb82d3b29104c97201226
Dominik Weber <d.weber@berufsschule.com>
2024-09-11 17:46:57 +0200

    Erster Upload

---
+++ 0_Vorstellung_Einrichtung_HelloWorld/Hallo Welt.py
@@ -0,0 +1,36 @@
+# Das ist ein Kommentar!
+# Das ist mein erstes Programm
+# Um das Programm auszuführen, müssen wir folgende
+# Schritte durchführen:
+# 1. Eingabeaufforderung öffnen: Start > Suche > cmd
+# 2. In Konsole eingeben: cd "PFAD_ZUM_ORDNER_WO_DATEI_LIEGT"
+# 3. In Konsole eingeben: dir 
+# 4. Programm ausführen: python "NAME_DER_DATEI"
+
+# Anweisungen werden von OBEN nach UNTEN abgearbeitet
+# Ein Python Programm nennt sich auch SKRIPT
+# In Python programmiert man mit ANWEISUNGEN
+
+print("Hallo Welt") # Eine Anweisung geht normalerweise über eine Zeile!
+print("Test2")
+print("Dominik Weber") # Das ist eine Zeichenkette (Wort/Satz)
+print(5)
+print(5+5) # Hier wird gleich eine Berechnung durchgeführt
+print("5+5") # Ausgabe: 5+5 als Text
+print(5*2) # 10
+print(5**2) # Ausgabe 5^2
+print("Hallo"*2) 
+print("Dominik " + "Weber") # Zusammenketten von Zeichenketten
+
+# Input ist das "Gegenteil" von print: Einlesen von der Konsole
+rufname = input("Bitte gebe deinen Namen ein: ")
+
+# = Zuweisungsoperator
+# Der Wert RECHTS vom = wird nach links kopiert
+# <------------------------------
+
+# Wir haben einen "Platzhalter" name eingeführt
+# und speichern den eingegebenen Name in dieser VARIABLE
+
+# Ausgabe der Variable auf der Konsole:
+print(rufname)
---
+++ README.md
@@ -0,0 +1,19 @@
+# Unterrichtsmaterialien für das Fach Programmierung (24/25)
+
+Herzlich Willkommen auf dem github-Repository für das Fach Programmierung an der Technikerschule Neumarkt!
+
+Hier finden Sie alle aktuellen Unterrichtsmaterialien (Programmcode, PDFs, ...) zum Unterricht. 
+
+Dieses Repository ist privat und read-only: Es ist nicht öffentlich zugänglich und Sie besitzen nur Leserechte.
+
+## Ordnerstruktur
+
+## Getting Started
+
+### Installation und Einrichtung von git
+
+### Erstellen eines github-Accounts
+
+## Workflow im Unterricht
+
+### Lokale Inhalte aktualisieren ("Pullen")
