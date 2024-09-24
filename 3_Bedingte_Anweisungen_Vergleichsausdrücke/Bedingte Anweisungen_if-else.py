54918ff931b4589ea7e7ce526769ad8b7a2a1ec4
Dominik Weber <d.weber@berufsschule.com>
2024-09-24 11:54:23 +0200

    Unterrichtsinhalte 24.09.

---
+++ 3_Bedingte_Anweisungen_Vergleichsausdrücke/Bedingte Anweisungen_if-else.py
@@ -0,0 +1,27 @@
+""" 
+24.09.24
+╔══════════════════════════════════╗
+║  BEDINGTE ANWEISUNGEN (IF-ELSE)  ║
+╚══════════════════════════════════╝
+""" 
+
+"""
+Arbeitsauftrag:
+===============
+Informieren Sie sich zu bedingten Anweisungen mithilfe des Fachbuchs.
+
+- Lesen Sie Kapitel 8.1 "if-Verzweigung" im Fachbuch
+
+Zeit: 8 Minuten
+"""
+
+
+
+
+
+
+
+
+
+
+
