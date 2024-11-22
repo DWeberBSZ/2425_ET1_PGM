2f0abe47bcefb45184ba2bccf4c053e7b18ed50f
Dominik Weber <d.weber@berufsschule.com>
2024-11-22 08:27:40 +0100

    Vorbereitung zur SA hinzugefügt (1)

--- 06_Sequenzielle_Datentypen_Listen/Übungsaufgaben/Vertiefungsaufgaben/01_Buchstabensalat_LSG.py
+++ 06_Sequenzielle_Datentypen_Listen/Übungsaufgaben/Vertiefungsaufgaben/01_Buchstabensalat_LSG.py
@@ -1,3 +1,14 @@
 txt = "DIenr  diesmt  Sdienrn eb eisstte  HLielhfree rz,u rd eSre lsbiscthh inlafceh  iumnmde rn aucnhs eürbee rofbleürssstieg  Mmaaxcihmte.." 
 
 print(txt[0::2])
+print(txt[1::2])
+
+# Nachtrag zum Palindrom:
+# Rentner -> Palindrom!
+
+# Man kann Strings zu klein oder zu groß konvertieren:
+txt_klein = txt.lower()
+txt_gross = txt.upper()
+
+print(txt_klein)
+print(txt_gross)
---
+++ 08_Vorbereitung_Schulaufgabe/A0_Prüfsumme_LSG.py
@@ -0,0 +1,28 @@
+"""
+Übungsaufgabe: Prüfsumme berechnen
+==================================
+
+Ziel dieser Aufgabe ist die Berechnung einer Prüfsumme von Kommazahlen innerhalb einer Liste.
+
+- Die Zahlen liegen als Zeichenkette kodiert nacheinander innerhalb einer Liste. 
+- Jede Kommazahl ist auf genau zwei Nachkommastellen angegeben.
+- Für die Berechnung der Prüfsumme sind nur folgende Ziffern relevant:
+    - Die Ziffer direkt VOR dem Dezimalpunkt
+    - Die Ziffer direkt NACH dem Dezimalpunkt
+
+AUFGABENSTELLUNG:
+- Schreiben Sie ein Python-Skript, das alle relevanten Ziffern aufaddiert und auf der Konsole ausgibt.
+
+"""
+
+values = ['78.22', '6.41', '18.24', '4.49', '44.82', '43.09', '643.77', '3.71', '29.38', 
+ '79.07', '95.54', '7.75', '24.65', '5.16', '7467.32', '6.68', '61.68', '91.61', 
+ '31.90', '334.08', '23.77', '28.26', '49.75', '915.84', '10.13', '1.87', '46.71', 
+ '7.91', '35.66', '6.83']
+
+sum = 0
+
+for value in values:
+    sum += int(value[-4]) + int(value[-2])
+
+print(f"Die Prüfsumme lautet: {sum}")
