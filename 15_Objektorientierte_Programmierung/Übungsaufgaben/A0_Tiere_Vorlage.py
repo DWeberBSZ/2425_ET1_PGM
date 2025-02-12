da9eca3246e5b6a5039f39d2d5f68be35148aa10
Dominik Weber <d.weber@berufsschule.com>
2025-02-12 12:21:02 +0100

    Objektorientierte Programmierung

---
+++ 15_Objektorientierte_Programmierung/Übungsaufgaben/A0_Tiere_Vorlage.py
@@ -0,0 +1,22 @@
+# 1. Klasse definieren
+class Tier:
+    # Attribute:
+    def __init__(self):
+        # name, gatting, gewicht, groesse
+        self.name = "Tier"
+        self.gattung = "Tiergattung"
+        self.gewicht = 0
+        self.groesse = 0.0
+
+    # Methoden:
+    def Beschreibung(self):
+        pass # TODO!
+
+    def Fuettern(self):
+        pass # TODO!
+
+    def Schlafen(self):
+        pass # TODO!
+
+# 2. Objekt von Klasse anlegen und Methoden aufrufen
+# TODO!
